6f7fbdfe5ab0e20112e472688594812c27887e83
Anders Kielsholm <anders@anigif.dk>
2020-05-01 22:34:31 +0200

    Use queue instead of making a new list each iteration

--- skipper-skeeto-path-finder/include/skipper-skeeto-path-finder/path_controller.h
+++ skipper-skeeto-path-finder/include/skipper-skeeto-path-finder/path_controller.h
@@ -5,6 +5,7 @@
 #include "skipper-skeeto-path-finder/common_state.h"
 
 #include <unordered_map>
+#include <queue>
 
 class PathController {
 public:
@@ -31,111 +32,109 @@ private:
     auto availableRooms = data->getRooms();
     availableRooms.erase(std::find(availableRooms.begin(), availableRooms.end(), originPath->getCurrentRoom()));
 
-    std::vector<std::vector<const Room *>> onGoingPaths{{}};
+    std::queue<std::vector<const Room *>> onGoingPaths;
+    onGoingPaths.push({});
     while (onGoingPaths.size() > 0) {
-      std::vector<std::vector<const Room *>> pathsToContinue;
+      std::vector<const Room *> subPath = onGoingPaths.front();
+      onGoingPaths.pop();
 
-      for (const auto &subPath : onGoingPaths) {
-        const Room *currentRoom;
-        if (subPath.empty()) {
-          currentRoom = originPath->getCurrentRoom();
-        } else {
-          currentRoom = subPath.back();
-        }
-
-        for (const auto &nextRoom : currentRoom->getNextRooms()) {
-
-          auto availableRoomIterator = std::find(availableRooms.begin(), availableRooms.end(), nextRoom);
-          if (availableRoomIterator == availableRooms.end()) {
-            // Room already reached or cannot be entered.
-            // We cannot have gotten here faster than the other steps getting here.
-            continue;
-          }
-
-          const Room *avaiableRoom = *availableRoomIterator;
-          availableRooms.erase(availableRoomIterator);
-
-          auto enterRoomResult = canEnterRoom(originPath, avaiableRoom);
-          if (enterRoomResult == EnterRoomResult::CannotEnter) {
-            continue;
-          }
-
-          std::vector<const Room *> newSubPath(subPath);
-          newSubPath.push_back(avaiableRoom);
-
-          if (!commonState->makesSenseToPerformActions(originPath, newSubPath)) {
-            continue;
-          }
-
-          if (enterRoomResult == EnterRoomResult::CanEnterWithTaskObstacle) {
-            Path newPath(*originPath);
-            newPath.enterRooms(newSubPath);
-            newPath.completeTask(avaiableRoom->taskObstacle);
-
-            performPossibleActions(&newPath);
-
-            if (commonState->submitIfDone(&newPath)) {
-              continue;
-            }
-
-            if (commonState->makesSenseToStartNewSubPath(&newPath)) {
-              newPath.depth++;
-              newPathCallback(std::move(newPath));
-            }
-
-            continue;
-
-          } else if (enterRoomResult != EnterRoomResult::CanEnter) {
-            throw std::exception("Unknown enter room result!");
-          }
-
-          auto possibleTasks = getPossibleTasks(originPath, avaiableRoom);
-          if (!possibleTasks.empty()) {
-            Path newPath(*originPath);
-            newPath.enterRooms(newSubPath);
-            newPath.completeTasks(possibleTasks);
-
-            newPath.pickUpItems(getPossibleItems(&newPath, avaiableRoom));
-
-            if (commonState->submitIfDone(&newPath)) {
-              continue;
-            }
-
-            if (commonState->makesSenseToStartNewSubPath(&newPath)) {
-              newPath.depth++;
-              newPathCallback(std::move(newPath));
-            }
-
-            continue;
-          }
-
-          auto possibleItems = getPossibleItems(originPath, avaiableRoom);
-          if (!possibleItems.empty()) {
-            Path newPath(*originPath);
-            newPath.enterRooms(newSubPath);
-            newPath.pickUpItems(possibleItems);
-
-            if (commonState->submitIfDone(&newPath)) {
-              continue;
-            }
-
-            if (commonState->makesSenseToStartNewSubPath(&newPath)) {
-              newPath.depth++;
-              newPathCallback(std::move(newPath));
-            }
-
-            continue;
-          }
-
-          if (!commonState->makesSenseToExpandSubPath(originPath, newSubPath)) {
-            continue;
-          }
-
-          pathsToContinue.push_back(newSubPath);
-        }
+      const Room *currentRoom;
+      if (subPath.empty()) {
+        currentRoom = originPath->getCurrentRoom();
+      } else {
+        currentRoom = subPath.back();
       }
 
-      onGoingPaths = pathsToContinue;
+      for (const auto &nextRoom : currentRoom->getNextRooms()) {
+
+        auto availableRoomIterator = std::find(availableRooms.begin(), availableRooms.end(), nextRoom);
+        if (availableRoomIterator == availableRooms.end()) {
+          // Room already reached or cannot be entered.
+          // We cannot have gotten here faster than the other steps getting here.
+          continue;
+        }
+
+        const Room *avaiableRoom = *availableRoomIterator;
+        availableRooms.erase(availableRoomIterator);
+
+        auto enterRoomResult = canEnterRoom(originPath, avaiableRoom);
+        if (enterRoomResult == EnterRoomResult::CannotEnter) {
+          continue;
+        }
+
+        std::vector<const Room *> newSubPath(subPath);
+        newSubPath.push_back(avaiableRoom);
+
+        if (!commonState->makesSenseToPerformActions(originPath, newSubPath)) {
+          continue;
+        }
+
+        if (enterRoomResult == EnterRoomResult::CanEnterWithTaskObstacle) {
+          Path newPath(*originPath);
+          newPath.enterRooms(newSubPath);
+          newPath.completeTask(avaiableRoom->taskObstacle);
+
+          performPossibleActions(&newPath);
+
+          if (commonState->submitIfDone(&newPath)) {
+            continue;
+          }
+
+          if (commonState->makesSenseToStartNewSubPath(&newPath)) {
+            newPath.depth++;
+            newPathCallback(std::move(newPath));
+          }
+
+          continue;
+
+        } else if (enterRoomResult != EnterRoomResult::CanEnter) {
+          throw std::exception("Unknown enter room result!");
+        }
+
+        auto possibleTasks = getPossibleTasks(originPath, avaiableRoom);
+        if (!possibleTasks.empty()) {
+          Path newPath(*originPath);
+          newPath.enterRooms(newSubPath);
+          newPath.completeTasks(possibleTasks);
+
+          newPath.pickUpItems(getPossibleItems(&newPath, avaiableRoom));
+
+          if (commonState->submitIfDone(&newPath)) {
+            continue;
+          }
+
+          if (commonState->makesSenseToStartNewSubPath(&newPath)) {
+            newPath.depth++;
+            newPathCallback(std::move(newPath));
+          }
+
+          continue;
+        }
+
+        auto possibleItems = getPossibleItems(originPath, avaiableRoom);
+        if (!possibleItems.empty()) {
+          Path newPath(*originPath);
+          newPath.enterRooms(newSubPath);
+          newPath.pickUpItems(possibleItems);
+
+          if (commonState->submitIfDone(&newPath)) {
+            continue;
+          }
+
+          if (commonState->makesSenseToStartNewSubPath(&newPath)) {
+            newPath.depth++;
+            newPathCallback(std::move(newPath));
+          }
+
+          continue;
+        }
+
+        if (!commonState->makesSenseToExpandSubPath(originPath, newSubPath)) {
+          continue;
+        }
+
+        onGoingPaths.push(newSubPath);
+      }
     }
   }
 
