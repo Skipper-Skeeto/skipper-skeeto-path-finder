36418f937f44cb8f08886825b499e1705b1978c8
Anders Kielsholm <anders@anigif.dk>
2021-01-12 23:10:38 +0100

    Check if we cannot create dir in Windows

--- skipper-skeeto-path-finder/src/file_helper.cpp
+++ skipper-skeeto-path-finder/src/file_helper.cpp
@@ -1,5 +1,6 @@
 #include "skipper-skeeto-path-finder/file_helper.h"
 
+#include <stdexcept>
 #include <string>
 
 #ifdef _WIN32
@@ -8,7 +9,10 @@
 
 void FileHelper::createDir(const char *dirName) {
 #ifdef _WIN32
-  CreateDirectoryA(dirName, nullptr);
+  auto result = CreateDirectoryA(dirName, nullptr);
+  if (result == ERROR_PATH_NOT_FOUND) {
+    throw std::runtime_error(std::string("Could not create ") + dirName + ", might be since intermediate directories do not exist");
+  }
 #else
   system((std::string("mkdir ") + dirName).c_str());
 #endif
